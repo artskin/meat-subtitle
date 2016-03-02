619d6c6a5264926b83f2cd6e239782ac32484354
Meathill <meathill@gmail.com>
2016-03-03 00:17:53 +0800

    感觉越写越顺了

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 /**
  * Created by meathill on 16/2/28.
  */
+'use strict';
+
 var gulp = require('gulp')
   , handlebars = require('gulp-handlebars')
   , wrap = require('gulp-wrap')
   , declare = require('gulp-declare')
   , fs = require('fs')
-  , script_reg = /<script([^>]*)>([\S\s]+)<\/script>/g;
+  , vinylBuffer = require('vinyl-buffer')
+  , source = require('vinyl-source-stream')
+  , _ = require('underscore')
+  , script_reg = /<script([^>]*)>([\S\s]+?)<\/script>/g;
 
 gulp.task('templates', function () {
   let promise = new Promise(function (resolve, reject) {
@@ -17,22 +22,31 @@ gulp.task('templates', function () {
       resolve(data);
     });
   });
-  promise.then(function (content) {
+  return promise.then(function (content) {
     content.replace(script_reg, function (match, attr, template) {
       attr = _.chain(attr.split(' '))
         .map(function (item) {
           let pair = item.split('=');
           if (pair.length === 1) {
             pair[1] = true;
+          } else {
+            pair[1] = pair[1].replace(/^"|"$/g, '');
           }
           return pair;
         })
         .object()
         .value();
       if (attr.type && attr.type.match(/handlebars-template/)) {
-
+        let stream = source(attr['data-name']);
+        stream.write(template.replace(/\s{2}|\n|\r/g, ''));
+        process.nextTick(function () {
+          stream.end();
+        });
+        stream
+          .pipe(vinylBuffer())
+          .pipe(gulp.dest('js'));
       }
+      return '';
     });
-
   });
 });
